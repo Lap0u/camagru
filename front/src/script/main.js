fede062049dd9285b9dfcbfb687fba4a835f9214
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-03-16 22:07:28 +0100

    navbar en cours

--- front/src/script/main.js
+++ front/src/script/main.js
@@ -1,1 +1,13 @@
-console.log('Hello from main.js')
+function checkLogin () {
+  var username = document.getElementById('username').value
+  var password = document.getElementById('password').value
+
+  if (username === 'admin' && password === 'admin') {
+    window.location.href = "./camagru.html";
+    console.log('Login success')
+  } else {
+    alert('Login fail')
+  }
+}
+
+document.getElementById("form-login").addEventListener('submit', function () { checkLogin() })

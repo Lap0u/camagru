9793204687ca4c810f05b067427f7dbffb54e173
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-03-23 13:38:18 +0100

    webcam div ok, edit a faire

--- front/src/script/camagru.js
+++ front/src/script/camagru.js
@@ -1,8 +1,14 @@
 document.getElementById('webcam-button').addEventListener('click', getWebcam)
 document.getElementById('upload-image').addEventListener('click', uploadImage)
+document.getElementById('save-picture').addEventListener('click', savePicture)
 document.getElementById('input_file').addEventListener('change', changeImage)
 
 createLastUploadGallery()
+createEditGallery()
+
+function savePicture () {
+  console.log('savePicture()')
+}
 
 function createLastUploadGallery () {
   const gallery = document.getElementById('gallery-images')
@@ -14,46 +20,49 @@ function createLastUploadGallery () {
   }
 }
 
-function changeImage () {
-  const input = document.getElementById('input_file')
-  const video = document.getElementById('webcam-screen')
-  const file = input.files[0]
-  console.log('changeImage()')
-  video.srcObject = null
-  //read the file and replace the poster
-  if (file['type'].split('/')[0] != 'image') {
-    alert('Please select an image file')
-    return
-  }
-  var fr = new FileReader()
-  fr.onload = function (e) {
-    video.poster = fr.result
-  }
-  fr.readAsDataURL(file)
-}
+function createEditGallery () {
 
-function uploadImage () {
-  document.getElementById('input_file').value = null
-  document.getElementById('input_file').click()
-}
 
-async function getWebcam () {
-  let stream = null
-  console.log('getWebcam()')
-
-  try {
-    stream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        facingMode: 'user',
-        width: { ideal: 650 },
-        height: { ideal: 650 }
-      }
-    })
+  function changeImage () {
+    const input = document.getElementById('input_file')
     const video = document.getElementById('webcam-screen')
-    video.srcObject = stream
-    video.onloadedmetadata = () => video.play()
-  } catch (err) {
-    alert('Please allow access to your webcam or upload a picture')
+    const file = input.files[0]
+    console.log('changeImage()')
+    video.srcObject = null
+    //read the file and replace the poster
+    if (file['type'].split('/')[0] != 'image') {
+      alert('Please select an image file')
+      return
+    }
+    var fr = new FileReader()
+    fr.onload = function (e) {
+      video.poster = fr.result
+    }
+    fr.readAsDataURL(file)
   }
-}
+
+  function uploadImage () {
+    document.getElementById('input_file').value = null
+    document.getElementById('input_file').click()
+  }
+
+  async function getWebcam () {
+    let stream = null
+    console.log('getWebcam()')
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          facingMode: 'user',
+          width: { ideal: 650 },
+          height: { ideal: 650 }
+        }
+      })
+      const video = document.getElementById('webcam-screen')
+      video.srcObject = stream
+      video.onloadedmetadata = () => video.play()
+    } catch (err) {
+      alert('Please allow access to your webcam or upload a picture')
+    }
+  }

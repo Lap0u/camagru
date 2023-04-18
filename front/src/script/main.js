dc1a6f1547f930431aa9e47bf223c6a4666f6890
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-04-18 12:43:22 +0200

    check login fetch dans le front mais le back existe pas encore

--- front/src/script/main.js
+++ front/src/script/main.js
@@ -1,13 +1,40 @@
+async function verifyCredentials (newUser) {
+  console.log("post user")
+  //doit send le password confirm
+  fetch('http://localhost:4000/login', {
+    method: 'POST',
+    body: JSON.stringify(newUser),
+    headers: { 'content-type': 'application/json' },
+  })
+    .then(response => {
+      if (response.ok === true) {
+        return response.json()
+      } else if (response.status === 400) {
+        return response.json().then(data => {
+          throw new Error(data.message)
+        })
+      }
+    })
+    .then(user => console.log(user))
+    // .then(window.location.href = "./camagru.html")
+    .catch(error => {
+      alert(error)
+    })
+}
+
 function checkLogin () {
   let username = document.getElementById('username').value
   let password = document.getElementById('password').value
 
-  if (username === 'admin' && password === 'admin') {
-    window.location.href = "./camagru.html"
-    console.log('Login success')
-  } else {
-    alert('Login fail')
+
+  const user = {
+    username: username,
+    password: password
   }
+  verifyCredentials(user)
 }
 
-document.getElementById("form-login").addEventListener('submit', function () { checkLogin() })
+document.getElementById("form-login").addEventListener('submit', event => {
+  event.preventDefault()
+  checkLogin()
+})
